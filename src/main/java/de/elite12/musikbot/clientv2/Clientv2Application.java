de3dadc2b42b23d59530b6e80c93028b4a15dcec
Sven Kirschbaum <sven@elite12.de>
2019-12-21 03:01:34 +0100

    Initial Commit

---
+++ src/main/java/de/elite12/musikbot/clientv2/Clientv2Application.java
@@ -0,0 +1,13 @@
+package de.elite12.musikbot.clientv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clientv2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Clientv2Application.class, args);
+    }
+
+}
---
+++ src/test/java/de/elite12/musikbot/clientv2/Clientv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.elite12.musikbot.clientv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clientv2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
